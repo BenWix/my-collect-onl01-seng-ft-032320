8af46fcc41d7c50a5943f6c5853e15920cad0be1
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 12:49:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,12 @@
 def my_collect(array)
-
+  if block_given?
+    i = 0 
+    while i < array.length do 
+      yield array[i]
+      i+= 1 
+    end 
+  else 
+    puts "No block given"
+  end 
 end 
 
